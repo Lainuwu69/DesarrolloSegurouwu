4fb9f46212fb45f680a7f3318057f29b32e86c4e
Lainuwu69 <sthevenqv@gmail.com>
2026-05-06 14:11:54 -0500

    Corrijo alerta B104 de Bandit

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ def hello():
         "status": "ok",
         "message": "Pipeline CI/CD funcionando",
         "env": "dev"
-    })
-
+})
 @app.route('/health')
 def health():
     return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # nosec B104 - Necesario para Docker/Render
